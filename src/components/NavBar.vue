9850b14c8c73671c6ba60a5ad9796163264c43fb
Antonio <tonioufa@gmail.com>
2021-10-17 23:16:11 +0100

    stae with pinia, connect to wallet

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -103,39 +103,13 @@
           <div class="flex-shrink-0">
             <button
               type="button"
+              :disabled="walletStore.walletData != null"
+              :class="
+                walletStore.walletData == null ? 'hover:bg-indigo-600' : ''
+              "
               @click="connectWallet()"
-              class="relative inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-500 hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-indigo-500"
+              class="relative inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-500  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-indigo-500"
             >
-              <!-- Heroicon name: solid/plus-sm -->
-              <!-- <svg
-                class="-ml-1 mr-2 h-5 w-5"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
-                  clip-rule="evenodd"
-                />
-              </svg> -->
-
-              <!-- <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="-ml-1 mr-2 h-5 w-5"
-                fill="currenColor"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M8.111 16.404a5.5 5.5 0 017.778 0M12 20h.01m-7.08-7.071c3.904-3.905 10.236-3.905 14.141 0M1.394 9.393c5.857-5.857 15.355-5.857 21.213 0"
-                />
-              </svg> -->
-
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 class="h-5 w-5 mr-2"
@@ -148,7 +122,14 @@
                   clip-rule="evenodd"
                 />
               </svg>
-              <span class="">Connect Wallet</span>
+              <span class="">{{
+                walletStore.walletData != null
+                  ? `Connected ${walletStore.walletData.slice(
+                      0,
+                      2
+                    )}...${walletStore.walletData.slice(-4)}`
+                  : `Connect Wallet`
+              }}</span>
             </button>
           </div>
         </div>
@@ -184,61 +165,6 @@
           >Calendar</a
         >
       </div>
-      <!-- <div class="pt-4 pb-3 border-t border-gray-700">
-        <div class="flex items-center px-5 sm:px-6">
-          <div class="flex-shrink-0">
-            <img
-              class="h-10 w-10 rounded-full"
-              src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-              alt=""
-            />
-          </div>
-          <div class="ml-3">
-            <div class="text-base font-medium text-white">Tom Cook</div>
-            <div class="text-sm font-medium text-gray-400">tom@example.com</div>
-          </div>
-          <button
-            type="button"
-            class="ml-auto flex-shrink-0 bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-          >
-            <span class="sr-only">View notifications</span>
-            <svg
-              class="h-6 w-6"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-              />
-            </svg>
-          </button>
-        </div>
-         <div class="mt-3 px-2 space-y-1 sm:px-3">
-          <a
-            href="#"
-            class="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700"
-            >Your Profile</a
-          >
-
-          <a
-            href="#"
-            class="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700"
-            >Settings</a
-          >
-
-          <a
-            href="#"
-            class="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700"
-            >Sign out</a
-          >
-        </div> 
-      </div> -->
     </div>
   </nav>
 </template>
@@ -246,11 +172,28 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 
+import { useWalletStore } from '@/stores/wallet'
+
 export default defineComponent({
   setup() {
-    const a = 0
+    const walletStore = useWalletStore()
+
+    const connectWallet = async () => {
+      try {
+        const data = await window.ethereum.request({
+          method: 'eth_requestAccounts',
+        })
+        console.log('data :>> ', data)
+        // this.userAddress = data[0]
+        walletStore.saveWalletData(data[0])
+        console.log('DApp connected to your wallet 💰')
+      } catch (error) {
+        console.error('Error connecting DApp to your wallet')
+      }
+    }
     return {
-      a,
+      connectWallet,
+      walletStore,
     }
   },
 })
